7b6193278d8a3af734053e455cda77ee86d0250e
7H3LaughingMan <7H3LaughingMan@proton.me>
2026-02-13 11:26:06 -0600

    Global Exports

--- build/exports.js
+++ build/exports.js
@@ -14,29 +14,23 @@ files.sort((a, b) => {
     const a_parts = a.split("/");
     const b_parts = b.split("/");
 
-    const a_folders = a_parts.slice(1, -1);
-    const a_file = a_parts.at(-1) ?? "";
+    for (let index = 0; index < Math.max(a_parts.length, b_parts.length); index++) {
+        const a_part = a_parts[index];
+        const b_part = b_parts[index];
 
-    const b_folders = b_parts.slice(1, -1);
-    const b_file = b_parts.at(-1) ?? "";
+        if (!a_part && !b_part) break;
 
-    for (let index = 0; index < Math.max(a_folders.length, b_folders.length); index++) {
-        const a_folder = a_folders[index];
-        const b_folder = b_folders[index];
+        if (!a_part || !b_parts) {
+            if (!a_part) return -1;
 
-        if (!a_folder && !b_folder) break;
-
-        if (!a_folder || !b_folder) {
-            if (!a_folder) return -1;
-
-            if (!b_folder) return 1;
+            if (!b_parts) return 1;
         }
 
-        const folder_compare = a_folder.localeCompare(b_folder);
-        if (folder_compare !== 0) return folder_compare;
+        const part_compare = a_part.localeCompare(b_part);
+        if (part_compare !== 0) return part_compare;
     }
 
-    return a_file.localeCompare(b_file);
+    return 0;
 });
 
 files = files.filter((value) => !value.endsWith(".svelte.d.ts"));
@@ -46,9 +40,6 @@ files.forEach((value, index, array) => {
 });
 
 files.unshift(
-    'import "foundry-types/global-external.d.mts";',
-    'import "./global.d.ts";',
-    "",
     'export type { ActionCost } from "./module/item/base/data/system.d.ts";',
     'export type { HitPointsStatistic } from "./module/system/statistic/hit-points.d.ts";',
     "",
@@ -56,4 +47,4 @@ files.unshift(
 
 files.push("");
 
-fs.writeFileSync("./src/index.d.ts", files.join("\n"));
+fs.writeFileSync("./src/global-exports.d.ts", files.join("\n"));
